b5c9897c9aca304b1e2b09d016f13d47ca27320e
EliyaFarhat <eliyahfarhat@gmail.com>
2024-04-12 17:58:24 -0400

    destroy reports and comments with account cancellation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bug_reports
+  has_many :bug_reports, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
